cd8e9061bc0a936e355c446721598977ad896714
rhare86 <rhare86@gmail.com>
2022-06-27 12:34:27 -0500

    HELP

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const express = require('express');
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-const inputCheck = require('./utils/inputCheck');
-
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -15,37 +12,16 @@ app.use(express.json());
 // Use apiRoutes
 app.use('/api', apiRoutes);
 
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
-
-// Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//                 VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-// db.query(sql, params, (err, result) => {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
-
-
-
-// Default response for any other requests (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
+// Default response for any other request (Not Found)
+app.use((req, res) => { 
+  res.status(404).end();
 });
 
 // Start server after DB connection
 db.connect(err => {
-    if (err) throw err;
-    console.log('Database connected.');
-app.listen(PORT, () => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
+  });
 });
