06123c5f3a808b8bcc9e0041c213409867db5e09
rhare86 <rhare86@gmail.com>
2022-06-27 19:43:42 -0500

    added voter routes

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,12 @@
 const mysql = require('mysql2');
 
-// Connect to database
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'LifenSkillen1)',
-      database: 'election'
-    },
-    console.log('Connected to the election database.')
-  );
-
-
-
-
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'LifenSkillen1)',
+  database: 'election'
+});
 
 module.exports = db;
